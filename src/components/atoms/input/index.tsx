62e599107ec996355b87fac7a6166a30c6505e66
YWTechIT <abcd8637@naver.com>
2021-08-19 22:02:27 +0900

    ADD Input Component

---
+++ src/components/atoms/input/index.tsx
@@ -0,0 +1,27 @@
+import { useCallback, useState } from "react";
+
+interface InputProps{
+  onInsert: (title: string) => void;
+}
+
+const Input = ({onInsert}: InputProps) => {
+  const [title, setTitle] = useState<string>("");
+
+  const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback((e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onInsert(title);
+    setTitle("");
+  }, [onInsert, title])
+ 
+  return (
+    <form onSubmit={onSubmit}>
+      <input onChange={handleChange} value={title} placeholder="할 일을 적어주세요."></input>
+    </form>
+  );
+};
+
+export default Input;
