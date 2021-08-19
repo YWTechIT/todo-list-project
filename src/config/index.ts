3ccac53036a3c9c34d3a007b87ec7f2518c26519
YWTechIT <abcd8637@naver.com>
2021-08-19 22:03:39 +0900

    ADD Config TODO_CONST
    처음 렌더링 될 때 config 값 선언

---
+++ src/config/index.ts
@@ -0,0 +1,15 @@
+import { TodoTypes } from "../types";
+
+export const TODO_CONST: TodoTypes[] = 
+[
+    {
+        id: "1",
+        title: "왜 사용하는지 알기",
+        checked: false
+    },
+    {
+        id: "2",
+        title: "타입스크립트 공부하기",
+        checked: false
+    }
+]
