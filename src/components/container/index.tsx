530603a3d54b3806224816297bcaf7a472907c87
YWTechIT <abcd8637@naver.com>
2021-08-19 22:02:32 +0900

    ADD Container Component

---
+++ src/components/container/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+interface ContainerProps {
+    children: React.ReactNode;
+}
+const ContainerWrapper = styled.main`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 64px auto;
+`
+
+const Container = ({children}: ContainerProps) => <ContainerWrapper>{children}</ContainerWrapper>
+
+export default Container;
