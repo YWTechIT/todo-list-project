4d25b53cfbcc6491837a81666f475a630b839944
YWTechIT <abcd8637@naver.com>
2021-08-22 22:23:18 +0900

    Change TodoGroupWrapper CSS

--- src/components/organisms/todoGroup/index.tsx
+++ src/components/organisms/todoGroup/index.tsx
@@ -1,6 +1,8 @@
 import { TodoTypes } from "../../../types";
 import Todo from "../../molecules/todo";
 import styled from "styled-components";
+import Footer from "../footer";
+import { Ol } from "../../../typhography/ol";
 
 interface Todos {
   todos: TodoTypes[];
@@ -8,22 +10,25 @@ interface Todos {
   isChecked: (id: string) => void;
 }
 
-const TodoWrapper = styled.article`
-  width: 680px;
-  border: 1px solid black;
-  padding: 10px 20px;
+const TodoGroupWrapper = styled.article`
+  width: 512px;
+  border: 2px solid #3d8ddc;
   margin: 32px 0px;
+  padding: 8px;
+  border-radius: 5px;
+  box-shadow: 0px 10px 15px rgba(0,0,0,0.1);
 `;
 
 const TodoGroup = ({ todos, onRemove, isChecked }: Todos) => {
   return (
-    <TodoWrapper>
-      <ol>
+    <TodoGroupWrapper>
+      <Ol>
         {todos.map((todo) => (
           <Todo key={todo.id} todo={todo} onRemove={onRemove} isChecked={isChecked}></Todo>
         ))}
-      </ol>
-    </TodoWrapper>
+      </Ol>
+      <Footer todos={todos} />
+    </TodoGroupWrapper>
   );
 };
 
