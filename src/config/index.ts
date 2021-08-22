a90d24ddfd31c606a42f41d7ce83f7fdeb81ddeb
YWTechIT <abcd8637@naver.com>
2021-08-22 22:22:40 +0900

    Add LOCAL_STORAGE_NAME

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,15 +1,1 @@
-import { TodoTypes } from "../types";
-
-export const TODO_CONST: TodoTypes[] = 
-[
-    {
-        id: "1",
-        title: "왜 사용하는지 알기",
-        checked: false
-    },
-    {
-        id: "2",
-        title: "타입스크립트 공부하기",
-        checked: false
-    }
-]
+export const LOCAL_STORAGE_NAME = "todos";
