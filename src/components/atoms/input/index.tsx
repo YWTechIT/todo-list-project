e2c95b3cae68d76c18058ad537b716f6f810c923
YWTechIT <abcd8637@naver.com>
2021-08-23 18:25:38 +0900

    ADD inputRef
    렌더링 이후 `input` 박스로 커서를 자동으로 옮기는 코드

--- src/components/atoms/input/index.tsx
+++ src/components/atoms/input/index.tsx
@@ -1,11 +1,21 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+import styled from "styled-components";
 
 interface InputProps{
   onInsert: (title: string) => void;
 }
 
+const InputStyle = styled.input`
+  width: 256px;
+  height: 24px;
+  font-size: 16px;
+  border: 1px solid ${(props) => props.theme.borderColor};
+  box-shadow: 0px 10px 15px rgba(0,0,0,0.1);
+`
+
 const Input = ({onInsert}: InputProps) => {
   const [title, setTitle] = useState<string>("");
+  const ref = useRef<HTMLInputElement>(null);
 
   const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value);
@@ -16,10 +26,14 @@ const Input = ({onInsert}: InputProps) => {
     !!title.trim() && onInsert(title.trim());
     setTitle("");
   }, [onInsert, title])
+
+  useEffect(() => {
+    if (ref.current) ref.current.focus();
+  }, [])
  
   return (
     <form onSubmit={onSubmit}>
-      <input onChange={handleChange} value={title} placeholder="할 일을 적어주세요."></input>
+      <InputStyle onChange={handleChange} value={title} placeholder="할 일을 적어주세요." ref={ref}/>
     </form>
   );
 };
